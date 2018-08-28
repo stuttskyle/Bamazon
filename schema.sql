26161598592c72fca11dc210836f3f97c8ebe2a6
Hunter Stutts <stuttskyle@gmail.com>
2018-08-27 21:34:58 -0500

    Made it more complete

---
+++ schema.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+    item_id INT(11) AUTO_INCREMENT NULL PRIMARY KEY,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price FLOAT (7,2) NULL,
+    stock_quantity INT NULL
+);
+
+-- ALTER TABLE products AUTO_INCREMENT = 1000;
+
+-- CREATE TABLE departments(
+--     department_id INT(11) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+--     department_name VARCHAR(30) NOT NULL,
+--     over_head_costs INT(11) NOT NULL
+-- );
+
+-- ALTER TABLE departments AUTO_INCREMENT = 2000;
---
+++ seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Costume", "Clothing", 14.99, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Candy Corn", "Food", 1.00, 3);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Halloween Soundtrack", "Electronics", 7.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Plastic Skeleton", "Decorations", 2.57, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Fog Machine", "Electronics", 200.01, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Pumpkin Carving Kit", "Decorations", 5.25, 1000);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Candy Bowl", "Food", 4.69, 80);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Rubber Bat", "Decorations", 8.39, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Glow Stix", "Decorations", 0.99, 200);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES("Wig/Mask", "Clothing", 10.00, 200);
