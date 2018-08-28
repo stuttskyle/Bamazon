6d0b9e4a3eff4c4b2d4db327e17966fd21b0a654
Hunter Stutts <stuttskyle@gmail.com>
2018-08-27 21:45:59 -0500

    Added more:

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,11 @@
+// Dependancies
 cosnt mysql = require("mysql");
 const inquirer = require("inquirer");
 
+// Stock Variable
+const stocl = [];
+
+// Database
 const connection = mysql.creatConnection({
     host: "localhost",
     port: 3306,
@@ -9,13 +14,19 @@ const connection = mysql.creatConnection({
     database: "bamazonDB"
 });
 
+// Connections to mySQL
 connection.connect(function(err){
     if (err) throw err;
-    runSearch();
 });
 
-function runSearch() {
-    inquirer.prompt({
-        
-    })
-}
+// Display all items
+function displayItems() {
+    connection.query('SELECT item_id, product_name, price FROM products', function (err, res) {
+        if (err) throw er;
+        res.forEach(i => {
+            stock.push(String(i.item_id));
+            console.log(`${i.item_id} -- ${i.product_name} \n PRICE: ${i.price}`)
+            console.log(`-----------------------------------`)
+        });
+    });
+};
